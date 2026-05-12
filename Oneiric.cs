6c4d89a953492714fc573b842aa6356fe281064c
Administrator <Administrator@DESKTOP-NCNLG39>
2026-05-12 15:41:07 +0800

    添加项目文件。

---
+++ Oneiric.cs
@@ -0,0 +1,17 @@
+using Terraria.ModLoader;
+
+namespace Oneiric
+{
+    //git:https://github.com/EPTENAS/Oneiric
+    public class Oneiric : Mod
+	{
+        public override void Load()
+        {
+           
+        }
+        public override void Unload()
+        {
+           
+        }
+    }
+}
